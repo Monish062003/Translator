c6e750b45aaa2720026d57b6fc77d9c5089e0bf7
MonishPro <mayekarmonish875@gmail.com>
2023-08-05 22:09:39 +0530

    Translator

---
+++ speech.js
@@ -0,0 +1,9 @@
+let speeches=(()=>{
+    alert("Script loaded")
+
+    //   everyone, Monish by this side. I built a google translator by combining 3 a p i's by google using vanilla j s for backend along with other frontend languages such as Html and Css along with tailwind css. I Hope you like my webpage
+      let synth=speechSynthesis;
+      const utterance = new SpeechSynthesisUtterance("Hello .");
+      synth.speak(utterance);
+    })
+    speeches()
